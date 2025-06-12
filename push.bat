fc65c8dadf330d2920564d708f1e778ffa776b12
TallanLem <38294527+TallanLem@users.noreply.github.com>
2025-06-13 01:06:51 +0300

    Auto update 13.06.2025-1:06:50,99-_

--- push.bat
+++ push.bat
@@ -1,13 +1,17 @@
 @echo off
-echo
+echo Пушим обновления...
 
-git add data
+git add -A
 
 for /f "tokens=1-4 delims=/ " %%a in ("%date% %time%") do (
-    set commit_msg=Auto update data %%a-%%b-%%c_%%d
+    set commit_msg=Auto update %%a-%%b-%%c_%%d
 )
 
 git commit -m "%commit_msg%"
+
+:: подтягиваем изменения с GitHub перед пушем
+git pull origin main --rebase
+
 git push origin main
 
 pause
